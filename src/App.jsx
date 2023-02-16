832312cf229fa8b4a840846f5f3a2c679a562f7f
alyoneek <alyonta03@mail.ru>
2023-02-16 21:11:52 +0700

    feat(routing): add basic routing
    add main routes and pages

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import reactLogo from '@/assets/react.svg';
-import './App.css';
-
-function App() {
-  return <div className='s'>{console.log(reactLogo)}</div>;
-}
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import RootLayout from '@/layouts/RootLayout';
+import Error from '@/pages/Error';
+import Gallery from '@/pages/Gallery';
+import Weather from '@/pages/Weather';
+import Collection from '@/pages/Collection';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Weather />,
+      },
+      {
+        path: 'weather',
+        element: <Weather />,
+      },
+      {
+        path: 'gallery',
+        element: <Gallery />,
+      },
+      {
+        path: 'gallery/:collectionId',
+        element: <Collection />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
