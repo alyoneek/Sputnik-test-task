bfa935f2d9da17b38895b139d35718fb40ce2e3d
alyoneek <alyonta03@mail.ru>
2023-02-17 20:45:13 +0700

    feat(component): add placeholder
    axios request for getting random photo as placeholder
    
    BREAKING CHANGE: delete strict mode

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 
 import { SearchContext } from '@/layouts/RootLayout';
 
 const Gallery = () => {
   const { searchValue } = React.useContext(SearchContext);
-  console.log(searchValue);
-  return <div>Gallery</div>;
+  const [placeholder, setPlaceholder] = React.useState(null);
+  //const [collection, setCollection] = React.useState(null);
+  const [isSearching, setIsSearching] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!placeholder) {
+      axios
+        .get(
+          'https://api.unsplash.com/photos/random?client_id=rvFHIBJq97ulHnhSkSL_5A6zq_N7RMivPQyVRChOQlM'
+        )
+        .then((response) => {
+          setPlaceholder(response.data.urls.regular);
+        });
+    }
+  }, []);
+  return (
+    !isSearching && <img src={placeholder} alt='' style={{ opacity: 0.5 }} />
+  );
 };
 
 export default Gallery;
