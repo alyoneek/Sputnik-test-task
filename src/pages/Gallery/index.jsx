8033edbc3b2ecc32d0664dd8fa15fddde6f233e0
alyoneek <alyonta03@mail.ru>
2023-02-18 01:43:12 +0700

    style(gallery): add placeholder styles
    placeholder_title has shadow, placeholder_image has width and height 100%

--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -3,13 +3,13 @@ import axios from 'axios';
 
 import { SearchContext } from '@/layouts/RootLayout';
 
+import styles from './Gallery.module.scss';
+
 const Gallery = () => {
   const { searchValue } = React.useContext(SearchContext);
   const [placeholder, setPlaceholder] = React.useState(null);
   //const [collection, setCollection] = React.useState(null);
   const [isSearching, setIsSearching] = React.useState(false);
-
-  console.log(searchValue);
 
   React.useEffect(() => {
     if (!placeholder) {
@@ -18,12 +18,26 @@ const Gallery = () => {
           'https://api.unsplash.com/photos/random?client_id=rvFHIBJq97ulHnhSkSL_5A6zq_N7RMivPQyVRChOQlM'
         )
         .then((response) => {
-          setPlaceholder(response.data.urls.regular);
+          setPlaceholder(response.data.urls.full);
         });
     }
   }, []);
+
   return (
-    !isSearching && <img src={placeholder} alt='' style={{ opacity: 0.5 }} />
+    !isSearching && (
+      <div className={styles.placeholder}>
+        <img
+          src={placeholder}
+          alt='gallery_placeholder'
+          className={styles.placeholder_image}
+        />
+        <div className={styles.placeholder_title}>
+          <h1>Gallery</h1>
+          <p>Search the pictures of a collection by its id.</p>
+          <p>More text.</p>
+        </div>
+      </div>
+    )
   );
 };
 
