3cbd8e39b3901ce539a45a8951535d44fce8ab21
alyoneek <alyonta03@mail.ru>
2023-02-19 15:57:10 +0700

    feat(weather): add api request for weather page
    in CurrentWeather component replace static data

--- src/components/Weather/CurrentWeather/index.jsx
+++ src/components/Weather/CurrentWeather/index.jsx
@@ -2,38 +2,43 @@ import React from 'react';
 
 import styles from './CurrentWeather.module.scss';
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
   return (
     <div className={styles.weather}>
       <div className={styles.main_features}>
         <div className={styles.left_col}>
-          <div className={styles.temperature}>32°</div>
+          <div className={styles.temperature}>{data.main.temp}°</div>
           <div className={styles.title}>
-            <div className={styles.name}>Sunny</div>
-            <img src='http://openweathermap.org/img/wn/10d@2x.png' alt='' />
+            <div className={styles.name}>{data.weather[0].main}</div>
+            <img
+              src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+              alt=''
+            />
           </div>
-          <div className={styles.description}>scattered clouds</div>
+          <div className={styles.description}>
+            {data.weather[0].description}
+          </div>
         </div>
         <div className={styles.right_col}>
-          <div className={styles.feels_like}>Feels like 37°</div>
-          <div className={styles.uvi}>UV index 2.55</div>
-          <div className={styles.clouds}>Cloud cover 40%</div>
+          <div className={styles.feels_like}>
+            Feels like {data.main.feels_like}°
+          </div>
         </div>
       </div>
       <div className={styles.extended_features}>
         <div className={styles.pressure}>
           <div className={styles.pressure_label}>Pressure</div>
-          <div className={styles.pressure_value}>1014mb</div>
+          <div className={styles.pressure_value}>{data.main.pressure}mb</div>
         </div>
 
         <div className={styles.visibility}>
           <div className={styles.visibility_label}>Visibility</div>
-          <div className={styles.visibility_value}>10000m</div>
+          <div className={styles.visibility_value}>{data.visibility}m</div>
         </div>
 
         <div className={styles.humadity}>
           <div className={styles.humadity_label}>Humadity</div>
-          <div className={styles.humadity_value}>65%</div>
+          <div className={styles.humadity_value}>{data.main.humidity}%</div>
         </div>
       </div>
     </div>
--- src/pages/Weather.jsx
+++ src/pages/Weather.jsx
@@ -4,7 +4,42 @@ import axios from 'axios';
 import CurrentWeather from '@/components/Weather/CurrentWeather';
 
 const Weather = () => {
-  return <CurrentWeather />;
+  const [date, setDate] = React.useState(new Date());
+  const [locationCoords, setLocationCoords] = React.useState(null);
+  const [location, setLocation] = React.useState(null);
+  const [weather, setWeather] = React.useState(null);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => setDate(new Date()), 1000);
+    return () => clearInterval(timer);
+  });
+
+  React.useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocationCoords({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  React.useEffect(() => {
+    const fetchWeather = async () => {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${locationCoords.lat}&lon=${locationCoords.lon}&appid=6fb7b9e3b3bb770125b23d4b54457037`
+      );
+      setLocation({
+        city: response.data.name,
+        country: response.data.sys.country,
+      });
+      setWeather(response.data);
+      console.log(response.data);
+    };
+
+    locationCoords && fetchWeather();
+  }, [locationCoords]);
+
+  return weather && <CurrentWeather data={weather} />;
 };
 
 export default Weather;
