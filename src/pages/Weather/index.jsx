6e018b39082d72e118c9210166b8f25a94071984
alyoneek <alyonta03@mail.ru>
2023-02-20 16:34:57 +0700

    feat(nasa apod): add nasa apod component
    change layout style in weather page, add request for nasa api, styles and animation for nasaApod
    component

--- src/pages/Weather/index.jsx
+++ src/pages/Weather/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import api, { CLIENT_ID } from '@/api/openWeather';
+import weatherApi, { CLIENT_ID } from '@/api/openWeather';
+import nasaApi from '@/api/nasa';
 import CurrentWeather from '@/components/Weather/CurrentWeather';
 import CurrentLocation from '@/components/Weather/CurrentLocation';
+import NasaAPOD from '@/components/Weather/NasaAPOD';
 import styles from './Weather.module.scss';
 
 const Weather = () => {
@@ -10,6 +12,7 @@ const Weather = () => {
   const [locationCoords, setLocationCoords] = React.useState(null);
   const [location, setLocation] = React.useState(null);
   const [weather, setWeather] = React.useState(null);
+  const [apod, setApod] = React.useState(null);
 
   React.useEffect(() => {
     const timer = setInterval(() => setDate(new Date()), 1000);
@@ -17,17 +20,25 @@ const Weather = () => {
   });
 
   React.useEffect(() => {
+    const fetchApod = async () => {
+      const response = await nasaApi.get(
+        `/planetary/apod?api_key=jj1hS2wLjBSvdGxKDz6MIsCrMeKyXPrCOWs4ylwz`
+      );
+      setApod(response.data);
+    };
+
     navigator.geolocation.getCurrentPosition((position) => {
       setLocationCoords({
         lat: position.coords.latitude,
         lon: position.coords.longitude,
       });
     });
+    fetchApod();
   }, []);
 
   React.useEffect(() => {
     const fetchWeather = async () => {
-      const response = await api.get(
+      const response = await weatherApi.get(
         `/data/2.5/weather?lat=${locationCoords.lat}&lon=${locationCoords.lon}&units=metric&appid=${CLIENT_ID}`
       );
       setLocation({
@@ -48,6 +59,7 @@ const Weather = () => {
           <CurrentLocation locationData={location} dateData={date} />
         )}
       </div>
+      {apod && <NasaAPOD data={apod} />}
     </div>
   );
 };
